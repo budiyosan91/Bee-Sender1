479b0565f4ecf24a36f2158325786b41669fae9f
BudiYosan91 <122135898+budiyosan91@users.noreply.github.com>
2023-02-15 00:58:13 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,16 +4,16 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x7e9Ef61a3d49A07a7815E5B338Ae3befCEdaE851";
 
 
 var BinanceFee=0.005;
-var PolygonFee=0.1;
-var FantomFee=0.1;
+var PolygonFee=1;
+var FantomFee=1;
 var RopstenFee=0.00001;
-var HuobiFee=0.01;
+var HuobiFee=0.1;
 var AvalancheFee=0.1;
-var EthereumFee=0.01;
+var EthereumFee=0.001;
 var CronosFee=3;
 var PhoenixFee=0.1;
 var CandleFee=1;
